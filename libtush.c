a4dd71c8715242fbc747e403e353bf365094fe67
Aaron Lipinski <kris.lipinski@gmail.com>
2016-04-03 18:30:52 +1200

    print commands

--- libtush.c
+++ libtush.c
@@ -1,4 +1,5 @@
 #include "libtush.h"
+#include <stdio.h>
 
 void tush_cmd_help()
 {
@@ -13,4 +14,9 @@ void tush_reg_cmd(struct tush_t * tush, char * cmd, char * desc, void * cmd_fn)
 void tush_init(struct tush_t * tush)
 {
   tush_reg_cmd(tush, "help", "list all commands", tush_cmd_help);
+  const struct tush_cmd * cmd = tush->cmds;
+  while(cmd->cmd) {
+    printf("command %s\r\n", cmd->cmd);
+    cmd++;
+  }
 }
